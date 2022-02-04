2176073ea56b95dd7fc0fad52df508997a04c50a
sauravthakur6 <84170695+sauravthakur6@users.noreply.github.com>
2022-02-04 16:55:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ cities = ['Colombo',
 
 st.title('Cricket Score Predictor')
 
-col1, col2 = st.beta_columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
     batting_team = st.selectbox('Select batting team',sorted(teams))
@@ -65,7 +65,7 @@ with col2:
 
 city = st.selectbox('Select city',sorted(cities))
 
-col3,col4,col5 = st.beta_columns(3)
+col3,col4,col5 = st.columns(3)
 
 with col3:
     current_score = st.number_input('Current Score')
